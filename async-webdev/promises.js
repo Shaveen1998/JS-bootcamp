a6953ed420095e4befcdbf0d468491cf53ee10e5
iamshaveenvf <shaveenfernando1@gmail.com>
2024-01-10 19:20:59 +0530

    promises2

--- async-webdev/promises.js
+++ async-webdev/promises.js
@@ -1,3 +1,38 @@
 //solution for callback hell (nested callbacks)
 
-const promise = new Promise();
+const fakeRequest = (url)=>{
+    
+return new Promise((resolved, rejected)=>{
+    const delay = Math.floor(Math.random()*4500)+500;
+
+    setTimeout(()=>{
+        if(delay>4000){
+            rejected('Error');
+        }else{
+            resolved(`here is the data from ${url}`)
+        }
+    },delay)
+})
+
+
+}
+
+
+//nested way as a sol to callback hell
+
+fakeRequest('/efef')
+.then((data)=>{
+    console.log(data)
+    return fakeRequest('/efef/page2')
+})
+.then(data=>{
+    console.log(data)
+    return fakeRequest('/efef/page3')
+})
+.then(data=>{
+    console.log(data)
+    return fakeRequest('/efef/page4')
+})
+.catch((err)=>{
+    console.log(err)
+})
